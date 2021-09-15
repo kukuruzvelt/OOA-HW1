961c3ef6d2aa120a854bc065bf1faf301317d0f1
kukuruzvelt <81823080+kukuruzvelt@users.noreply.github.com>
2021-09-15 22:54:09 +0300

    Update main.py

--- Lab1.2/main.py
+++ Lab1.2/main.py
@@ -5,9 +5,3 @@ firstNum = sys.argv[2]
 secondNum = sys.argv[3]
 str = "int(" + firstNum + ").__" + action + "__(int(" + secondNum + "))"
 print(eval(str))
-"""
-sub-разница
-add-сумма
-truediv-деление
-mul-умножение
-"""
