6e130d69dfd8bca5c0d593f76920da3b50c8c361
kukuruzvelt <81823080+kukuruzvelt@users.noreply.github.com>
2021-09-22 18:31:55 +0300

    Update main.py

--- Lab1.1/main.py
+++ Lab1.1/main.py
@@ -3,4 +3,11 @@ import sys
 firstNum = sys.argv[1]
 action = sys.argv[2]
 secondNum = sys.argv[3]
-print(eval(firstNum+action+secondNum))
+try:
+    print(eval(firstNum + action + secondNum))
+except ZeroDivisionError as ex:
+    print("ERROR: division by 0")
+except NameError as ex:
+    print("ERROR: wrong input")
+except SyntaxError as ex:
+    print("ERROR: wrong operation")
