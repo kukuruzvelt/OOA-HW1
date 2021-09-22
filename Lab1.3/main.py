c453e5a14913813176896457e1716c69085adfde
kukuruzvelt <81823080+kukuruzvelt@users.noreply.github.com>
2021-09-22 18:32:34 +0300

    Update main.py

--- Lab1.3/main.py
+++ Lab1.3/main.py
@@ -11,25 +11,24 @@ def is_formula(string, sign):  # determines if an expressions is a formula
     for i in string:
         if i in numbers:
             if passed == 0:
-                checked_string = checked_string + sign
-            checked_string = checked_string + i
+                checked_string += sign
+            checked_string += i
+            passed += 1
+        elif i in signs:
+            if passed == 0 and sign:
+                return [False, "None"]
             passed = passed + 1
-        else:
-            if i in signs:
-                if passed == 0:
-                    if i in signs:
-                        return [False, "None"]
-                passed = passed + 1
-                result = is_formula(string[passed:], i)
-                if result[0]:
-                    checked_string = checked_string + result[1]
-                    return [True, checked_string]
-                else:
-                    return [False, "None"]
+            result = is_formula(string[passed:], i)
+            if result[0]:
+                checked_string = checked_string + result[1]
+                return [True, checked_string]
             else:
                 return [False, "None"]
+        else:
+            return [False, "None"]
     return [True, checked_string]
 
 
-result = is_formula(inputString, "")
-print("result:", result[0], ",", eval(result[1]))
+finalResult = is_formula(inputString, "")
+print("result:", finalResult[0], ",", eval(finalResult[1]))
+
